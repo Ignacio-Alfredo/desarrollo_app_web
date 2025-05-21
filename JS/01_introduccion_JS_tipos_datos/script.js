32046c7ba1a65c315267fa8390dbedde589d7aff
Ignacio_Alfredo <garridoignacio022@gmail.com>
2025-05-21 18:30:40 -0400

    Funciones Java

--- JS/01_introduccion_JS_tipos_datos/script.js
+++ JS/01_introduccion_JS_tipos_datos/script.js
@@ -6,7 +6,15 @@ console.log("Conexión establecida con JS!");
 let num = 25; 
 const numDec = 3.14;
 let cad = "Hola, ¿Cómo Estas?";
+let Bool = true; 
+let arry = [1,2,3]; 
+let obj = {
+    nombre: "Alberto", 
+    edad:19
+    };
+let und;
 
+let Null = null;
 
 function numeroEntero(){
     return "Variable Númerica: " + num;
@@ -20,8 +28,33 @@ function cadenaTexto (){
     return "Cadena de Texto: " + cad;
 }
 
+function numeroBooleano(){
+    return "Variable Booleano: "+ Bool
+}
 
-//Impresión en Consola
+function numeroArryse(){
+    return "Variable Arryse: "+ arry
+}
+
+function numeroName(){
+    return "Variable Object: "+ obj.nombre + ", " + obj.edad; 
+}
+
+function numeroInde(){
+    return "Variable Undefined: "+ und
+}
+
+
+function numeroNull(){
+    return "Variable Null: "+ Null
+}
+
+
 console.log(numeroEntero()); 
 console.log(numeroDecimal());
 console.log(cadenaTexto());
+console.log(numeroBooleano());
+console.log(numeroArryse());
+console.log(numeroName());
+console.log(numeroInde());
+console.log(numeroNull());
